9bdf9aeaab5b79f8234a0700313dbc409b6bcb76
kusano <kusano@users.noreply.github.com>
2023-02-23 07:12:13 +0900

    パラメタ調整
    6188945
    26,658,330,110

--- A.cpp
+++ A.cpp
@@ -143,6 +143,74 @@ public:
         vector<int> HY,
         function<int(int x, int y, int P)> excavate
     ) {
+        switch (C)
+        {
+        case 1:
+            LEVEL_START = 12;
+            LEVEL_END = 8;
+            PROSPECT_P = 10;
+            PROSPECT_N = 15;
+            BREAK_DECAY = 74;
+            BREAK_ADD = 7;
+            break;
+        case 2:
+            LEVEL_START = 12;
+            LEVEL_END = 8;
+            PROSPECT_P = 28;
+            PROSPECT_N = 7;
+            BREAK_DECAY = 46;
+            BREAK_ADD = 19;
+            break;
+        case 4:
+            LEVEL_START = 16;
+            LEVEL_END = 8;
+            PROSPECT_P = 50;
+            PROSPECT_N = 5;
+            BREAK_DECAY = 33;
+            BREAK_ADD = 45;
+            break;
+        case 8:
+            LEVEL_START = 13;
+            LEVEL_END = 9;
+            PROSPECT_P = 41;
+            PROSPECT_N = 6;
+            BREAK_DECAY = 54;
+            BREAK_ADD = 41;
+            break;
+        case 16:
+            LEVEL_START = 15;
+            LEVEL_END = 9;
+            PROSPECT_P = 98;
+            PROSPECT_N = 4;
+            BREAK_DECAY = 70;
+            BREAK_ADD = 42;
+            break;
+        case 32:
+            LEVEL_START = 12;
+            LEVEL_END = 9;
+            PROSPECT_P = 89;
+            PROSPECT_N = 4;
+            BREAK_DECAY = 86;
+            BREAK_ADD = 25;
+            break;
+        case 64:
+            LEVEL_START = 9;
+            LEVEL_END = 9;
+            PROSPECT_P = 70;
+            PROSPECT_N = 6;
+            BREAK_DECAY = 74;
+            BREAK_ADD = 48;
+            break;
+        case 128:
+            LEVEL_START = 12;
+            LEVEL_END = 9;
+            PROSPECT_P = 49;
+            PROSPECT_N = 5;
+            BREAK_DECAY = 94;
+            BREAK_ADD = 54;
+            break;
+        }
+
         // 掘削済み。
         bool excavated[N][N] = {};
 
--- search_param.py
+++ search_param.py
@@ -5,23 +5,32 @@ import sys
 
 pwn.context.log_level = "error"
 
+C = int(sys.argv[1])
+
+params = []
+for l in open("seeds.txt"):
+  params += [list(map(int, l.split()))]
+
 def objective(trial):
-  level_start = trial.suggest_int("level_start", 2, 14)
-  level_end = trial.suggest_int("level_end", 2, 14)
-  prospect_p = trial.suggest_int("prospect_p", 1, 1000)
-  prospect_n = trial.suggest_int("prospect_n", 1, 100)
+  level_start = trial.suggest_int("level_start", 2, 16)
+  level_end = trial.suggest_int("level_end", 2, 16)
+  prospect_p = trial.suggest_int("prospect_p", 1, 256)
+  prospect_n = trial.suggest_int("prospect_n", 1, 20)
   break_decay = trial.suggest_int("break_decay", 1, 99)
-  break_add = trial.suggest_int("break_add", 1, 1000)
+  break_add = trial.suggest_int("break_add", 1, 100)
 
   score = 0
-  for i in range(100):
-    s = pwn.process(f"./A {level_start} {level_end} {prospect_p} {prospect_n} {break_decay} {break_add} < tools/in/{i:04d}.txt > /dev/null", shell=True)
-    score += int(s.readline().split()[-1])
-    s.close()
-  return score
+  n = 0
+  for i in range(len(params)):
+    if params[i][3]==C:
+      s = pwn.process(f"./A {level_start} {level_end} {prospect_p} {prospect_n} {break_decay} {break_add} < in/{i:04d}.txt > /dev/null", shell=True)
+      score += int(s.readline().split()[-1])
+      n += 1
+      s.close()
+  return score/n
 
 study = optuna.create_study(
-  study_name="ahc018_1",
+  study_name=f"ahc018_3_{C}",
   storage="sqlite:///db.sqlite3",
   load_if_exists=True,
   direction="minimize")
